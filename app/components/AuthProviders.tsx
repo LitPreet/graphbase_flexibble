3804edf7ee989bd33d526084a193025deebb46d4
preet006 <preetbhardwaj146@gmail.com>
2023-09-14 21:24:47 +0530

    final commit

--- app/components/AuthProviders.tsx
+++ app/components/AuthProviders.tsx
@@ -1,10 +1,39 @@
+'use client';
+import { useState, useEffect } from "react";
+import { getProviders, signIn } from "next-auth/react";
+
+type Provider = {
+    id: string,
+    name:string,
+    type:string,
+    signInUrl: string,
+    callbackUrl: string,
+    signinUrlParams?:Record<string, string> | null;
+
+}
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
-  return (
+    const[providers, setProviders] = useState<Providers | null>(null);
+
+useEffect(() => {
+const fetchProviders = async () => {
+    const res:any = await getProviders();
+    setProviders(res);
+    console.log(res);
+}
+fetchProviders();
+},[])
+
+if(providers) {
+   return (
     <div>
-      Auth
+        {Object.values(providers).map((provider:Provider, i) => (
+            <button key={i} onClick={() => signIn(provider?.id)}>{provider.id}</button>
+        ))}
     </div>
-  )
+   ) 
+}
 }
 
 export default AuthProviders
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,9 +2,12 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { NavLinks } from '@/constants'
 import AuthProviders from './AuthProviders'
+import { getCurrentUser } from '@/lib/session'
+import { signOut } from 'next-auth/react'
+import ProfileMenu from './ProfileMenu'
 
-const Navbar = () => {
-  const session = null;
+const Navbar = async () => {
+  const session = await getCurrentUser();
   return (
     <nav className='flexBetween navbar'>
       <div className='flex-1 flexStart gap-10'>
@@ -24,15 +27,17 @@ const Navbar = () => {
       </div>
 
       <div className="flexCenter gap-4">
-       { session ? (
+       { session?.user ? (
         <>
-        UserPhoto 
+       <ProfileMenu session={session}/>
         <Link href="/create-project">
         Share Work
         </Link>
+        {/* <button type='button' className='text-sm' onClick={signOut}>SignOut</button> */}
         </>
        ) : (
         <AuthProviders />
+
        )}
 
       </div>
